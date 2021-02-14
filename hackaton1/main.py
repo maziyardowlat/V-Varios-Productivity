26605481228b3ad453d1232379f84a92fe649963
Maziyar Dowlatabadibazaz <79039790+maziyardowlat@users.noreply.github.com>
2021-02-14 12:03:05 -0700

    Update main.py

--- hackaton1/main.py
+++ hackaton1/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('index.html', title = "Harshal's Sexy App")
+	return render_template('index.html', title = "V'Varios Productivity App")
     
     
 
